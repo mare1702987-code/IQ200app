3d6c2a84a0bc8daceea12ef07114e6572c72ee85
mare1702987-code <mare1702987@gmail.com>
2026-07-24 21:45:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const DEEPSEEK_API_KEY = process.env.DEEPSEEK_API_KEY;
 
